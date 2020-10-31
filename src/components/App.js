e51e92d66e5be9afd4f085893d2edcd54154bee6
Sudip Ghosh <ghoshsudip084@gmail.com>
2020-11-01 00:13:30 +0530

    Change 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,50 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date(),
+      display: ""
+    };
+  }
+  componentDidMount() {
+    console.log("mounted");
+    this.intervalId = setInterval(
+      () => this.getTimeString(this.state.time),
+      1000
+    );
+  }
 
-        return(
-            <>
-               
-            </>
-        )
+  getTimeString(d) {
+    let hrs = d.getHours();
+    let suffix = hrs >= 12 ? "PM" : "AM";
+    if (hrs > 12) {
+      hrs -= 12;
     }
-}
+    let min = d.getMinutes();
+    if (min < 10) {
+      min = "0" + min;
+    }
+    let sec = d.getSeconds();
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    let newTime = hrs + ":" + min + ":" + sec + " " + suffix;
+    this.setState({ time: new Date(), display: newTime });
+  }
 
+  componentWillUnmount() {
+    console.log("unmounted");
+    clearInterval(this.intervalId);
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.display}</h3>
+      </div>
+    );
+  }
+}
 
 export default App;
